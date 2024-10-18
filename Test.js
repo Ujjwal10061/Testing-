80c1372a63c67407590e38eb4a161958b013f3d9
Ujjwal10061 <UJJIPTL@GMAIL.COM>
2024-10-18 18:06:19 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,5 +1,19 @@
-var total = 0;  
-let unusedVar = 5;  
-for (let i = 0; i < cart.length; i++) {
-  total += cart[i].price;
+function factorial(n) {
+    if (n < 0) {
+        return "Error: Negative numbers don't have a factorial";
+    }
+    function recurseFactorial(x) {
+        if (x === 0 || x === 1) {
+            return 1;
+        }
+        return x * recurseFactorial(x - 1);
+    }
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+
+    return recurseFactorial(n) + result;  
 }
+
+console.log(factorial(5)); 
